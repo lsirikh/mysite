38256e7bc748c6ea6e685db6ffc6c11f46adfe99
lsirikh <lsirikh@naver.com>
2018-10-10 17:33:25 +0000

    aws test

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ with open(os.path.join(BASE_DIR, 'www_dir', 'secret_key.txt')) as f:
 DEBUG = True
 #DEBUG = False #Operational Mode
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-209-40-171.ap-northeast-2.compute.amazonaws.com']
 
 
 
